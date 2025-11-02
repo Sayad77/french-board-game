e958c60b14ffbd70529951c201bf20a97b7de098
Sayadali <sayadali.madarbukus@my-digital-school.org>
2025-11-02 15:22:04 +0100

    Create questions.js

---
+++ server/data/questions.js
@@ -0,0 +1,64 @@
+{
+  "themes": {
+    "food": {
+      "level1": [
+        { "id": "f1", "type": "translation", "prompt_en": "apple", "answer_fr": "pomme", "audio": "audio/food/pomme.mp3" },
+        { "id": "f2", "type": "translation", "prompt_en": "bread", "answer_fr": "pain", "audio": "audio/food/pain.mp3" },
+        { "id": "f3", "type": "translation", "prompt_en": "cheese", "answer_fr": "fromage", "audio": "audio/food/fromage.mp3" },
+        { "id": "f4", "type": "translation", "prompt_en": "water", "answer_fr": "eau", "audio": "audio/food/eau.mp3" }
+      ],
+      "level2": [
+        { "id": "f5", "type": "fill", "prompt_en": "I eat a ___ (croissant)", "answer_fr": "croissant", "audio": "audio/food/croissant.mp3" },
+        { "id": "f6", "type": "translation", "prompt_en": "butter", "answer_fr": "beurre", "audio": "audio/food/beurre.mp3" },
+        { "id": "f7", "type": "translation", "prompt_en": "salt", "answer_fr": "sel", "audio": "audio/food/sel.mp3" },
+        { "id": "f8", "type": "translation", "prompt_en": "knife", "answer_fr": "couteau", "audio": "audio/food/couteau.mp3" }
+      ],
+      "level3": [
+        { "id": "f9", "type": "fill", "prompt_en": "Could I have ___, please? (a menu)", "answer_fr": "le menu", "audio": "audio/food/menu.mp3" },
+        { "id": "f10", "type": "translation", "prompt_en": "delicious", "answer_fr": "délicieux", "audio": "audio/food/delicieux.mp3" },
+        { "id": "f11", "type": "translation", "prompt_en": "taste", "answer_fr": "goût", "audio": "audio/food/gout.mp3" },
+        { "id": "f12", "type": "translation", "prompt_en": "spicy", "answer_fr": "épicé", "audio": "audio/food/epice.mp3" }
+      ]
+    },
+    "travel": {
+      "level1": [
+        { "id": "t1", "type": "translation", "prompt_en": "train", "answer_fr": "train", "audio": "audio/travel/train.mp3" },
+        { "id": "t2", "type": "translation", "prompt_en": "ticket", "answer_fr": "billet", "audio": "audio/travel/billet.mp3" },
+        { "id": "t3", "type": "translation", "prompt_en": "hotel", "answer_fr": "hôtel", "audio": "audio/travel/hotel.mp3" },
+        { "id": "t4", "type": "translation", "prompt_en": "map", "answer_fr": "carte", "audio": "audio/travel/carte.mp3" }
+      ],
+      "level2": [
+        { "id": "t5", "type": "fill", "prompt_en": "I need a ___ to the city center (taxi)", "answer_fr": "taxi", "audio": "audio/travel/taxi.mp3" },
+        { "id": "t6", "type": "translation", "prompt_en": "departure", "answer_fr": "départ", "audio": "audio/travel/depart.mp3" },
+        { "id": "t7", "type": "translation", "prompt_en": "arrival", "answer_fr": "arrivée", "audio": "audio/travel/arrivee.mp3" },
+        { "id": "t8", "type": "translation", "prompt_en": "luggage", "answer_fr": "bagage", "audio": "audio/travel/bagage.mp3" }
+      ],
+      "level3": [
+        { "id": "t9", "type": "fill", "prompt_en": "Where is the ___? (platform)", "answer_fr": "la plateforme", "audio": "audio/travel/platform.mp3" },
+        { "id": "t10", "type": "translation", "prompt_en": "reservation", "answer_fr": "réservation", "audio": "audio/travel/reservation.mp3" },
+        { "id": "t11", "type": "translation", "prompt_en": "passport", "answer_fr": "passeport", "audio": "audio/travel/passeport.mp3" },
+        { "id": "t12", "type": "translation", "prompt_en": "departure gate", "answer_fr": "porte d'embarquement", "audio": "audio/travel/porte_embarquement.mp3" }
+      ]
+    },
+    "daily": {
+      "level1": [
+        { "id": "d1", "type": "translation", "prompt_en": "hello", "answer_fr": "bonjour", "audio": "audio/daily/bonjour.mp3" },
+        { "id": "d2", "type": "translation", "prompt_en": "goodbye", "answer_fr": "au revoir", "audio": "audio/daily/aurevoir.mp3" },
+        { "id": "d3", "type": "translation", "prompt_en": "please", "answer_fr": "s'il vous plaît", "audio": "audio/daily/silvousplait.mp3" },
+        { "id": "d4", "type": "translation", "prompt_en": "thank you", "answer_fr": "merci", "audio": "audio/daily/merci.mp3" }
+      ],
+      "level2": [
+        { "id": "d5", "type": "fill", "prompt_en": "I live in ___ (Paris)", "answer_fr": "Paris", "audio": "audio/daily/paris.mp3" },
+        { "id": "d6", "type": "translation", "prompt_en": "friend", "answer_fr": "ami", "audio": "audio/daily/ami.mp3" },
+        { "id": "d7", "type": "translation", "prompt_en": "family", "answer_fr": "famille", "audio": "audio/daily/famille.mp3" },
+        { "id": "d8", "type": "translation", "prompt_en": "work", "answer_fr": "travail", "audio": "audio/daily/travail.mp3" }
+      ],
+      "level3": [
+        { "id": "d9", "type": "fill", "prompt_en": "I would like ___ (to order)", "answer_fr": "commander", "audio": "audio/daily/commander.mp3" },
+        { "id": "d10", "type": "translation", "prompt_en": "weather", "answer_fr": "temps", "audio": "audio/daily/temps.mp3" },
+        { "id": "d11", "type": "translation", "prompt_en": "appointment", "answer_fr": "rendez-vous", "audio": "audio/daily/rendezvous.mp3" },
+        { "id": "d12", "type": "translation", "prompt_en": "difficult", "answer_fr": "difficile", "audio": "audio/daily/difficile.mp3" }
+      ]
+    }
+  }
+}
